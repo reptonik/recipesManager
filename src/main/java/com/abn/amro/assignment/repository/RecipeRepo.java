b1899be45b582cf06bfd8791177f83cffcc494cb
Or Kucher <reptonik@gmail.com>
2023-12-04 19:56:53 +0100

    Add Specification to Repo

--- src/main/java/com/abn/amro/assignment/repository/RecipeRepo.java
+++ src/main/java/com/abn/amro/assignment/repository/RecipeRepo.java
@@ -2,6 +2,7 @@ package com.abn.amro.assignment.repository;
 
 import com.abn.amro.assignment.model.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RecipeRepo extends JpaRepository<Recipe, Long> {
+public interface RecipeRepo extends JpaRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
 }
