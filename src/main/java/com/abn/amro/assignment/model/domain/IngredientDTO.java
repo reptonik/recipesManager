2bede4011a331120668e4c15b2bcac85aa932b59
Or Kucher <reptonik@gmail.com>
2023-12-04 19:57:11 +0100

    Add Schema mapping

--- src/main/java/com/abn/amro/assignment/model/domain/IngredientDTO.java
+++ src/main/java/com/abn/amro/assignment/model/domain/IngredientDTO.java
@@ -1,11 +1,20 @@
 package com.abn.amro.assignment.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class IngredientDTO {
+    @Schema(description = "Ingredient ID", example = "1")
     private Long id;
+    @Schema(description = "Ingredient title", example = "Salt")
     @NotBlank
     private String title;
+
+    @JsonIgnore
+    public boolean hasId() {
+        return this.id != null && this.id > 0;
+    }
 }
--- src/main/java/com/abn/amro/assignment/model/domain/RecipeDTO.java
+++ src/main/java/com/abn/amro/assignment/model/domain/RecipeDTO.java
@@ -1,6 +1,8 @@
 package com.abn.amro.assignment.model.domain;
 
-import com.abn.amro.assignment.model.entity.Ingredient;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -10,15 +12,32 @@ import java.util.Set;
 
 @Data
 public class RecipeDTO {
+    @Schema(description = "Recipe ID", example = "1")
     private Long id;
+    @Schema(description = "Recipe title", example = "Vegetarian Pasta")
     @NotBlank
     @Size(min = 4, max = 50)
     private String title;
+    @Schema(description = "Number of servings", example = "4")
     @NotBlank
     private Integer servings;
+    @Schema(description = "Recipe instructions", example = "Boil pasta and add vegetables.")
     @NotBlank
     @Size(min = 5, max = 255)
     private String instructions;
+    @Schema(description = "Flag indicating whether the recipe is vegetarian", example = "true", implementation = Boolean.class)
     private Boolean isVegetarian;
-    private Set<Ingredient> recipeIngredients = new HashSet<>();
+    @ArraySchema(
+            schema = @Schema(implementation = IngredientDTO.class),
+            minItems = 1,
+            uniqueItems = true,
+            arraySchema = @Schema(description = "Ingredients in the recipe")
+    )
+    private Set<IngredientDTO> recipeIngredients = new HashSet<>();
+
+
+    @JsonIgnore
+    public boolean hasId() {
+        return this.id != null && this.id > 0;
+    }
 }
